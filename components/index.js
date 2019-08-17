0853a37fc5eb76cab0bfab516f6a4706b6e272bb
Devesh Rawat <user1@users-MacBook-Pro.local>
2019-08-17 13:28:37 +0530

    first commit

---
+++ components/index.js
@@ -0,0 +1,6 @@
+import CardItem from "./CardItem";
+import City from "./City";
+import Filters from "./Filters";
+import Icon from "./Icon";
+
+export { CardItem, City, Filters, Icon };
---
+++ screens/index.js
@@ -0,0 +1,5 @@
+import HomeScreen from "./Home";
+import MatchesScreen from "./Matches";
+import MessageScreen from "./Message";
+import ProfileScreen from "./Profile";
+export { HomeScreen, ProfileScreen, MessageScreen, MatchesScreen };
